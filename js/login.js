25846713ee449dd9a1825b1df3757d1269995e91
emiliogarciaoliver <pgarciaoliver@udesa.edu.ar>
2025-11-19 15:32:58 -0300

    cambios en saludo.js

--- js/login.js
+++ js/login.js
@@ -3,24 +3,27 @@ let form_login = document.querySelector(".form-login");
 form_login.addEventListener("submit", function(e){
     e.preventDefault();
 
+
     let email = this.email.value;
     let password = this.password.value;
 
     if (email === "" || password === "") {
-        alert("Por favor, completa estos campos")
+        alert("Por favor, completa estos campos");
     }
-
-    let userObj = {
+     else if (password.length < 6) {
+        alert("La contraseña debe tener almenos 6 caracteres ");
+     }
+     else {
+            let userObj = {
         email: email,
         password: password
-    }
-    
+    };
     let userString = JSON.stringify(userObj)
-
-    //guardar en session storage
+    
     sessionStorage.setItem("dataUser", userString);
 
-    //redirigir al perfil que se envie el formulario
-    this.form_login
+    this.submit();
     
- })
+}
+    
+})
